7dd239ae458953bc06f9132a01fe1b94ab523ae0
Victor Loh <victorlohwk@gmail.com>
2018-10-05 13:23:51 +0800

    some activities

---
+++ app/src/main/java/com/example/vlwk/invent/bActivity.java
@@ -0,0 +1,13 @@
+package com.example.vlwk.invent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
---
+++ app/src/main/java/com/example/vlwk/invent/cActivity.java
@@ -0,0 +1,13 @@
+package com.example.vlwk.invent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+    }
+}
